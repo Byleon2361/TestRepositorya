aafe3b68d6088d178066db59156bc67127391643
Major <n.boywalenko@yandex.ru>
2024-12-13 21:49:01 +0700

    5

--- docker_wrapper.sh
+++ docker_wrapper.sh
@@ -5,19 +5,19 @@ DOCKER_REGISTRY=$2
 
 function build_image {
     echo "Building Docker image $DOCKER_IMAGE..."
-    /usr/bin/docker build -t $DOCKER_IMAGE .
+    build -t $DOCKER_IMAGE .
 }
 
 function publish_image {
     echo "Publishing Docker image $DOCKER_IMAGE to $DOCKER_REGISTRY..."
-    /usr/bin/docker tag $DOCKER_IMAGE $DOCKER_REGISTRY/$DOCKER_IMAGE
-    /usr/bin/docker push $DOCKER_REGISTRY/$DOCKER_IMAGE
+    tag $DOCKER_IMAGE $DOCKER_REGISTRY/$DOCKER_IMAGE
+    push $DOCKER_REGISTRY/$DOCKER_IMAGE
 }
 
 function deploy_container {
     echo "Deploying container from image $DOCKER_REGISTRY/$DOCKER_IMAGE..."
-    /usr/bin/docker stop my-app || true && /usr/bin/docker rm my-app || true
-    /usr/bin/docker run -d --name my-app -p 80:80 $DOCKER_REGISTRY/$DOCKER_IMAGE
+    stop my-app || true && /usr/bin/docker rm my-app || true
+run -d --name my-app -p 80:80 $DOCKER_REGISTRY/$DOCKER_IMAGE
 }
 
 case "$3" in
