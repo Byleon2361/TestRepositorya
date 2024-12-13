80b0762173e816d5c1ab259120f3b22addbc636d
Major <n.boywalenko@yandex.ru>
2024-12-13 22:10:49 +0700

    6

--- docker_wrapper.sh
+++ docker_wrapper.sh
@@ -5,18 +5,18 @@ DOCKER_REGISTRY=$2
 
 function build_image {
     echo "Building Docker image $DOCKER_IMAGE..."
-    /usr/bin/docker build -t $DOCKER_IMAGE .
+     build -t $DOCKER_IMAGE .
 }
 
 function publish_image {
     echo "Publishing Docker image $DOCKER_IMAGE to $DOCKER_REGISTRY..."
-    /usr/bin/docker tag $DOCKER_IMAGE $DOCKER_REGISTRY/$DOCKER_IMAGE
-    /usr/bin/docker push $DOCKER_REGISTRY/$DOCKER_IMAGE
+     tag $DOCKER_IMAGE $DOCKER_REGISTRY/$DOCKER_IMAGE
+    push $DOCKER_REGISTRY/$DOCKER_IMAGE
 }
 
 function deploy_container {
     echo "Deploying container from image $DOCKER_REGISTRY/$DOCKER_IMAGE..."
-    stop my-app || true && /usr/bin/docker rm my-app || true
+    stop my-app || true &&  rm my-app || true
     run -d --name my-app -p 80:80 $DOCKER_REGISTRY/$DOCKER_IMAGE
 }
 
