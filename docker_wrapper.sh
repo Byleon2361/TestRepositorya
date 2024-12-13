7e208ef430567e04c4d7cb7424d84863efd63aaf
Major <n.boywalenko@yandex.ru>
2024-12-13 22:25:51 +0700

    cool

--- docker_wrapper.sh
+++ docker_wrapper.sh
@@ -5,20 +5,21 @@ DOCKER_REGISTRY=$2
 
 function build_image {
     echo "Building Docker image $DOCKER_IMAGE..."
-    /usr/bin/docker build -t $DOCKER_IMAGE .
+    $(which docker) build -t $DOCKER_IMAGE .
 }
 
 function publish_image {
     echo "Publishing Docker image $DOCKER_IMAGE to $DOCKER_REGISTRY..."
-    /usr/bin/docker tag $DOCKER_IMAGE $DOCKER_REGISTRY/$DOCKER_IMAGE
-    /usr/bin/docker push $DOCKER_REGISTRY/$DOCKER_IMAGE
+    $(which docker) tag $DOCKER_IMAGE $DOCKER_REGISTRY/$DOCKER_IMAGE
+    $(which docker) push $DOCKER_REGISTRY/$DOCKER_IMAGE
 }
 
 function deploy_container {
     echo "Deploying container from image $DOCKER_REGISTRY/$DOCKER_IMAGE..."
-    stop my-app || true && /usr/bin/docker rm my-app || true
-    run -d --name my-app -p 80:80 $DOCKER_REGISTRY/$DOCKER_IMAGE
+    $(which docker) stop my-app || true && $(which docker) rm my-app || true
+    $(which docker) run -d --name my-app -p 80:80 $DOCKER_REGISTRY/$DOCKER_IMAGE
 }
+
 
 case "$3" in
     build)
