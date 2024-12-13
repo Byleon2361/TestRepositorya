27a6c6b5a716102fca6702387ee0652bbc445356
Major <n.boywalenko@yandex.ru>
2024-12-13 22:15:53 +0700

    9

--- docker_wrapper.sh
+++ docker_wrapper.sh
@@ -4,7 +4,6 @@ DOCKER_IMAGE=$1
 DOCKER_REGISTRY=$2
 
 function build_image {
-    apt install docker
     echo "Building Docker image $DOCKER_IMAGE..."
     /usr/bin/docker build -t $DOCKER_IMAGE .
 }
